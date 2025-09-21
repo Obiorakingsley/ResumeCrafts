8e7c0b906e9e2ba50cc1f448d2d0911c8e874ae2
Obiora Kingsley <kingsleyobiora527@gmail.com>
2025-09-21 06:14:44 +0100

    Update input and label in resume/skills

--- src/app/(build)/resume/skills/page.tsx
+++ src/app/(build)/resume/skills/page.tsx
@@ -10,20 +10,22 @@ const page = () => {
         action=""
         className="form dark:text-slate-300 text-black dark:bg-black/80 shadow-lg bg-slate-50 text-center p-8 rounded-xl shadow-slate-400/10 grid gap-4 align-center border-2 border-slate-400/20 w-full max-w-xl"
       >
-        <label htmlFor="fullName">
-          Full Name
+        <label htmlFor="skills">
+          <span className="text-sm">
+            Add your skills (e.g, Project Management, Python, JavaScript, React)
+          </span>
           <input
-            id="fullName"
-            name="fullname"
+            id="skills"
+            name="skills"
             type="text"
-            placeholder="e.g. John Doe"
+            placeholder="e.g. React, Figma"
           />
         </label>
-        <div className="border-2 border-slate-50/50"></div>
+        <div className="border-2 dark:border-slate-50/20 border-slate-300/40"></div>
 
         <Link
           className="place-self-end my-2 bg-indigo-500 transition-colors hover:bg-indigo-600 py-1 px-3 rounded-md text-slate-50"
-          href="resume/skills"
+          href="resume/experience"
         >
           <button>Next</button>
         </Link>
